8df279b8665cbc642e379e88feb8ab7b6fc6de81
fay22ekanem <ekanem2004@gmail.com>
2025-11-07 09:36:42 +0100

    my first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+//Assignment on Js
+
+22 //expressions
+
+// 1. arithmetic operator
+
+// addition
+let age = 120 + 5;
+console.log(age);
+// substraction
+let myage = '10' - '5';
+console.log(myage);
+// division
+let yourage = 6 / 2;
+console.log(yourage);
+// multiplication
+let hisage = 5 * 5;
+console.log(hisage);
+// modulus
+let herage = 10 % 3;
+console.log(herage);
+// exponentiation
+let theirage = 2 ** 3;
+console.log(theirage);
+
+// 2. assignment operator
+
+let x = 10;
+x += 5; // x = x + 5
+console.log(x);
+x -= 3; // x = x - 3
+console.log(x);
+x *= 2; // x = x * 2
+console.log(x);
+x /= 4; // x = x / 4
+console.log(x); 
+
+// 3. comparison operator
+let a = 10;
+let b = 20;
+
+// equal to
+console.log(a == b); 
+// not equal to
+console.log(a != b);
+// greater than
+console.log(a > b);
+// less than
+console.log(a < b);
+// greater than or equal to
+console.log(a >= b);
+// less than or equal to
+console.log(a <= b);
+//=== strict equal to
+console.log(a === b);
+
+// 4 logical operator
+let p = true;
+let q = false;
+
+// AND operator
+console.log(p && q);
+// OR operator
+console.log(p || q);
+// NOT operator
+console.log(!p);
+
+
+// 5. string operator
+
+let firstName = "John";
+let lastName = "Doe";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+// 6. number operator
+
+let num1 = 15;
+let num2 = 4;
+let sum = num1 + num2;
+console.log(sum);
+
